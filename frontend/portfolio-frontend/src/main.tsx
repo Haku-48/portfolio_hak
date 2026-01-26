10b4a63901ee72598803582c185bdda30c0f4ff3
Guillaume Goetghebeur <guillaume.goetghebeur.etu@univ-lille.fr>
2026-01-26 19:34:58 +0100

    Avancements sur le Home

--- frontend/portfolio-frontend/src/main.tsx
+++ frontend/portfolio-frontend/src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+/* Importing style */
 import "./style/index.css";
+import "./style/home.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- frontend/portfolio-frontend/src/pages/Home.tsx
+++ frontend/portfolio-frontend/src/pages/Home.tsx
@@ -1,10 +1,10 @@
 /** Main page of the Portfolio */
-
+import Hero from "../components/sections/Hero.tsx";
 
 function Home() {
     return (
         <main className="page">
-            
+            <Hero />
         </main>
     )
 }
